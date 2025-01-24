548299f7e14b9f8f3f22f9b8b20d9b8666b2d154
Cameron H <enderman9523@gmail.com>
2025-01-24 13:46:33 -0700

    Python Data Library Base

---
+++ DataLib/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='lib',
+    packages=find_packages(include=['lib']),
+    version='0.1.0',
+    description='My first Python library',
+    author='Me',
+)
