bf21a3ca9b7b2286d6e391ca342acad85bdd7560
Lexi-jh <lexi.j.hanlon@gmail.com>
2025-01-31 16:21:59 -0700

    More robust function, correctly handles disconnecting

--- GroundStation/Game_Controller.py
+++ GroundStation/Game_Controller.py
@@ -1,25 +1,58 @@
+import sys
+import os
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = '1' #hides welcome prompt
 import pygame
 
-pygame.init()
-pygame.joystick.init()
 
-if pygame.joystick.get_count() == 0:
-    print("No joystick connected!")
-else:
-    joystick = pygame.joystick.Joystick(0)
-    joystick.init()
-    print("Joystick name:", joystick.get_name())
-    print("Number of axes:", joystick.get_numaxes())
+os.environ["SDL_JOYSTICK_ALLOW_BACKGROUND_EVENTS"] = '1' #allows events to be updated when window not in focus
+pygame.init() #initialize pygame class
+pygame.joystick.init() #initialize joystick class
 
-    # Example event loop:
-    running = True
-    while running:
+found_mess_printed = False #to keep track of if the no joystick message was printed once already
+run = True
+
+while run:
+    if pygame.joystick.get_count() == 0: #if joystick not connected
+        found = False
+
+        if not found_mess_printed:
+            print("No joystick found... Waiting...")
+            found_mess_printed = True
+
         for event in pygame.event.get():
-            if event.type == pygame.JOYAXISMOTION:
-                x_axis = joystick.get_axis(0)
-                y_axis = joystick.get_axis(1)
-                print("Axes:", x_axis, y_axis)
+            if event.type == pygame.JOYDEVICEADDED: #if controlled connected
+                print("Controller connected!")
+                controller = pygame.joystick.Joystick(0) #only registers the first on connected
+                controller.init() #initialize controller
+                found = True
+
+            elif event.type == pygame.JOYDEVICEREMOVED: #if controller uninitialized or removed
+                print("Controller disconnected...")
+
+            elif event.type == pygame.QUIT: #if program exited
+                run = False
+                pygame.quit()
+                sys.exit
+                break
+
+            elif found:
+                break
+
+    else:
+        found_mess_printed = False
+        controller = pygame.joystick.Joystick(0)
+        controller.init()
+        left_xaxis, left_yaxis, right_xaxis, right_yaxis, left_trig, right_trig = [controller.get_axis(i) for i in range(controller.get_numaxes())]
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                run = False
+            
+            elif event.type == pygame.JOYAXISMOTION:
+                axis_index = event.axis
+                val = event.value
+  
 
 pygame.quit()
+
+
