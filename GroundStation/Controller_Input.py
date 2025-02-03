4269418ea398e122433798911173c8e1bd24e992
Lexi-jh <lexi.j.hanlon@gmail.com>
2025-02-03 12:51:06 -0700

    Class structure for controller input

---
+++ GroundStation/Controller_Input.py
@@ -0,0 +1,9 @@
+import pygame
+
+class ControllerReader:
+    def __init__(self, pos, ind): #initialize
+        self.pos = pos
+        self.ind = ind
+
+    def _string_(self):
+        return f""
