6fc92be5cfcb003e338b9edb4aae574fb3f32763
Cameron H <enderman9523@gmail.com>
2025-02-07 13:26:40 -0700

    PacketSpeed Changes

--- PacketSpeed/Computer/PacketStruct.py
+++ PacketSpeed/Computer/PacketStruct.py
@@ -1,3 +1,4 @@
+import time as time
 import numpy as np
 
 class ControlPacket:
@@ -10,10 +11,57 @@ class ControlPacket:
         for i in self.packet:
             data += str(i) + sep
         return data
-        
 
 
-# Control1 = ControlPacket(1)
+class TimePacket:
+    def __init__(self,ImportedTimes):
+        if (str(ImportedTimes).__len__() > 0):
+            self.times = ImportedTimes
+        else:
+            self.times = str(time.time_ns())
+
+    def addtime(self):
+        self.times = self.times + ' ' + str(time.time_ns())
+
+    def printTimes(self):
+        tempstring = ""
+        for i in self.times:
+            if (i != ' '):
+                tempstring += i
+            else:
+                print(tempstring)
+                tempstring = ""
+        print(tempstring)
+    def printDelta(self):
+        times = []
+        tempstring = ""
+        for i in self.times:
+            if (i != ' '):
+                tempstring += i
+            else:
+                times = np.append(times, float(tempstring))
+                tempstring = ""
+        times = np.append(times, float(tempstring))
+        for i in range(np.size(times)):
+            if (i != 0):
+                print(times[i] - times[i-1])
+            else:
+                print(0)
 
 
-# print(Control1.encode())
+# test1 = TimePacket("")
+# test1.printTimes()
+
+# time.sleep(1)
+
+# test1.addtime()
+# test1.printTimes()
+
+# test2 = TimePacket(test1.times)
+# time.sleep(1)
+# test2.addtime()
+# test2.printTimes()
+
+# test2.printDelta()
+
+
