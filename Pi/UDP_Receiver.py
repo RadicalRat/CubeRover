759c46f3a4971108d699bc1ba802d8e8e4663071
Lexi-jh <lexi.j.hanlon@gmail.com>
2025-02-07 11:44:24 -0700

    creaton of udp receiver file on pi

---
+++ Pi/UDP_Receiver.py
@@ -0,0 +1,17 @@
+import socket
+import pickle
+
+class receive_UDP:
+    def _init_(self):
+        self.pi_ip = "10.42.0.1"
+        self.port = 65432
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((self.pi_ip, self.port))
+
+    def receive_data(self):
+        rec_data, _ = self.sock.recvfrom(1024) #buffer size in bytes
+        data = pickle.loads(rec_data)
+        return data
+    
+    def close(self):
+        self.sock.close()
