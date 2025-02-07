8fd098a22b3f841b4c03fcbc28683b2c7f2fceed
Lexi-jh <lexi.j.hanlon@gmail.com>
2025-02-07 13:24:30 -0700

    fixed init bug in udp_communication and updated class_test

--- GroundStation/UDP_communication.py
+++ GroundStation/UDP_communication.py
@@ -7,15 +7,15 @@ pi_ip = "10.42.0.1"
 comp_Port = 65432
 
 class UDPsender:
-    def _init_(self, host = pi_ip, port = comp_Port):
-        self.host = host
-        self.port = port
+    def __init__(self):
+        self.host = pi_ip
+        self.port = comp_Port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def send_data(self, data):
         try:
             message = pickle.dumps(data) #to send more versatile data
-            self.sock.sendto(message.encode(), (self.host, self.port))
+            self.sock.sendto(message, (self.host, self.port))
         except Exception as e:
             print(f"Error sending data: {e}")
     
--- GroundStation/class_test.py
+++ GroundStation/class_test.py
@@ -4,14 +4,15 @@ from UDP_communication import UDPsender
 import time
 
 controller = ControllerReader() #initiliaze instance of class
-
 controller.connect() #connect controller
+
+server = UDPsender()
 
 while True:
     if controller.controller is not None:
         data = controller.get_input() #returns list
         if data is not None:
-            UDPsender.send_data(data)
+            server.send_data(data)
     else:
         controller.connect() #try to connect controller again
 
