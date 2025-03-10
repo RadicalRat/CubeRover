17583bd43aa1eac85ac197828a0a4b09ed9873b5
Lexi-jh <lexi.j.hanlon@gmail.com>
2025-03-09 18:26:19 -0600

    Moving controller imnput and main functions for groundcontroller to main branch

---
+++ GroundStation/Main.py
@@ -0,0 +1,27 @@
+from Controller_Input import ControllerReader
+from UDP_communication import UDPsender
+import time
+
+controller = ControllerReader() #initiliaze instance of class
+udp_client = UDPsender()
+
+controller.connect() #connect controller
+
+try:
+    while True:
+        if controller.controller is not None:
+            data = controller.get_input() #returns list
+            if data is not None:
+                UDPsender.send_data(data) #send data over wifi
+        else:
+            controller.connect() #try to connect controller again
+
+        time.sleep(.05) #eventually change to match slowest frequency
+
+
+except KeyboardInterrupt: #when program stopped, properly close programs
+    controller.close()
+    UDPsender.close()
+
+
+
