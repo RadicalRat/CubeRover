1f6eebf6517e7dee97eb2514795545f64d51efd2
Lexi-jh <lexi.j.hanlon@gmail.com>
2025-03-10 13:50:21 -0600

    removing obsolete files

--- DataLib/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name='functions',
-    packages=find_packages(include=['functions']),
-    version='0.1.0',
-    description='Data library for serial transmission',
-    author='Cameron H',
-)
