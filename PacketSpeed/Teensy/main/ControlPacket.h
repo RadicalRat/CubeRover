43171d7faf48ca4e602d9c547db0478dc6c469da
Cameron H <enderman9523@gmail.com>
2025-03-12 13:26:23 -0600

    Teensy Input Logic

--- PacketSpeed/Teensy/main/ControlPacket.h
+++ PacketSpeed/Teensy/main/ControlPacket.h
@@ -4,6 +4,7 @@
 #include <RoboClaw.h>
 #include <elapsedMillis.h>
 
+elapsedMillis univTimer = 0;
 
 
 class ControlPacket { // Basic controlpacket parent class
@@ -11,10 +12,13 @@ class ControlPacket { // Basic controlpacket parent class
     ControlPacket() {}; // initializer -> does nothing
     virtual ~ControlPacket(); // destructor -> deletes _dataArr
     virtual void resolve(RoboClaw * RC1, RoboClaw * RC2) = 0; // resolve function -> resolves the packet
+    virtual bool fulfilled() = 0;
   protected:
     void populate(float * data); // populates the _dataArr array
     size_t _dataLength; // holds the length of data
     int * _dataArr; // holds the data to be resolved
+    RoboClaw * _RC1;
+    RoboClaw * _RC2;
 };
 
 // ControlPacket Implementations:
@@ -22,18 +26,21 @@ class Raw : public ControlPacket {
   public:
     Raw(float * data);
     void resolve(RoboClaw * RC1, RoboClaw * RC2) final;
+    bool fulfilled() final;
 };
 
 class VelPID : public ControlPacket {
  public:
    VelPID(float * data);
    void resolve(RoboClaw * RC1, RoboClaw * RC2) final;
+   bool fulfilled() final;
 };
 
 class PosPID : public ControlPacket {
  public:
    PosPID(float * data);
    void resolve(RoboClaw * RC1, RoboClaw * RC2) final;
+   bool fulfilled() final;
 };
 
 // IMPLEMENTATIONS
@@ -49,8 +56,6 @@ void ControlPacket::populate(float *data) { // deep copys data to _dataArr
   for (size_t i = 0; i < _dataLength; i++) {
       _dataArr[i] = data[i];
   }
-
-
 }
 
 // Implementations for child class Raw (for raw speed control)
@@ -62,10 +67,16 @@ Raw::Raw(float * data) { // initializes the raw class
 
 
 void Raw::resolve(RoboClaw * RC1, RoboClaw * RC2) { // drives the motors based on the 4 data values initialized
+  _RC1 = RC1;
+  _RC2 = RC2;
   RC1->ForwardBackwardM1(0x80, _dataArr[0]); 
   RC1->ForwardBackwardM2(0x80, _dataArr[1]);
   RC2->ForwardBackwardM1(0x80, _dataArr[2]);
   RC2->ForwardBackwardM2(0x80, _dataArr[3]);
+}
+
+bool Raw::fulfilled() {
+  return true;
 }
 
 
@@ -78,10 +89,17 @@ VelPID::VelPID(float * data) { // initilizes the Velocity PID speed control pack
 
 // TODO -> convert to qpps
 void VelPID::resolve(RoboClaw * RC1, RoboClaw * RC2) { // sets all motors to go at speed denoted by the only number in data
+  _RC1 = RC1;
+  _RC2 = RC2;
   RC1->SpeedM1(0x80, _dataArr[0]);
   RC1->SpeedM2(0x80, _dataArr[0]);
   RC2->SpeedM1(0x80, _dataArr[0]);
   RC2->SpeedM2(0x80, _dataArr[0]);
+  univTimer = 0;
+}
+
+bool VelPID::fulfilled() {
+  return true;
 }
 
 
@@ -94,10 +112,20 @@ PosPID::PosPID(float * data) { // initilizes the POSPID Class, for position cont
 
 // TODO -> convert to qpps
 void PosPID::resolve(RoboClaw * RC1, RoboClaw * RC2) { // tells all motors to go _dataArr[0] distance at _dataArr[1] speed
+  _RC1 = RC1;
+  _RC2 = RC2;
+  RC1->SetEncM1(0x80, 0);
+  RC1->SetEncM2(0x80, 0);
+  RC2->SetEncM1(0x80, 0);
+  RC2->SetEncM2(0x80, 0);
   RC1->SpeedDistanceM1(0x80, _dataArr[0], _dataArr[1]);
   RC1->SpeedDistanceM2(0x80, _dataArr[0], _dataArr[1]);
   RC2->SpeedDistanceM1(0x80, _dataArr[0], _dataArr[1]);
   RC2->SpeedDistanceM2(0x80, _dataArr[0], _dataArr[1]);
+}
+
+bool PosPID::fulfilled() { // checks to see if all motors have traveled distance
+    return true;
 }
 
 
--- PacketSpeed/Teensy/main/main.ino
+++ PacketSpeed/Teensy/main/main.ino
@@ -1,7 +1,11 @@
+#include <RingBuf.h>
+#include <elapsedMillis.h>
+
 #include <SerialTransfer.h>
 #include <RoboClaw.h>
 #include "ControlPacket.h"
 #include <Arduino.h>
+
 
 // Robot Parameters
 float WheelDiam = 15;
@@ -23,7 +27,6 @@ void setup() {
   // Init serial ports for PI / computer communication
   Serial.begin(38400); // set to higher baud rate later if needed
   rx.begin(Serial);
-  Serial.print("Serial Init");
 
   // Init serial ports for roboclaws
   ROBOCLAW_1.begin(38400);
@@ -41,64 +44,80 @@ void setup() {
   // ROBOCLAW_1.SetM2PositionPID(uint8_t address, float kp, float ki, float kd, uint32_t kiMax, uint32_t deadzone, uint32_t min, uint32_t max);
   // ROBOCLAW_2.SetM1PositionPID(uint8_t address, float kp, float ki, float kd, uint32_t kiMax, uint32_t deadzone, uint32_t min, uint32_t max);
   // ROBOCLAW_2.SetM2PositionPID(uint8_t address, float kp, float ki, float kd, uint32_t kiMax, uint32_t deadzone, uint32_t min, uint32_t max);
-  Serial.print("Waiting");
   digitalWrite(13,HIGH);
 }
 
 
-void loop() {
+ControlPacket * control = nullptr;
+RingBuf<ControlPacket*, 20> packetBuff;
 
-  if (rx.available()) { // TODO -> if packet is "done" -- marked by a flag -> delete and move on. if not, decode 1 packet & add to buffer, send 1 telem packet, check for done, wait some seconds
-    ControlPacket * control = SerialDecode();
-    control->resolve(&ROBOCLAW_1, &ROBOCLAW_2);
-    delete control;
-    Serial.println("Completed");
+void loop() { // Stuff to loop over 
+
+  
+
+  if (control == nullptr) {   // checks if there is currently a control packet commanding the rover, if yes:
+    if (!packetBuff.isEmpty()) {    // checks the packet buffer to see if there is a command in queue, if yes:
+      packetBuff.pop(control);    // adds queued command to control pointer
+      control->resolve(&ROBOCLAW_1, &ROBOCLAW_2);   // resolves control pointer command
+    } else if (rx.available()) {    // if no commands in packetBuffer, check serial port, if yes:
+      control = SerialDecode();   // adds serial buffer command to control pointer
+      control->resolve(&ROBOCLAW_1, &ROBOCLAW_2);   // resolves control pointer command
+    }
+  } else {    // if control packet is currently commanding rover:
+    if (control->fulfilled()) {   // is the packet done? If yes:
+      delete control;   // delete control packet
+      control = nullptr;    // set control packet to nullptr
+    } else {    // if not done:
+      if (rx.available()) {   // check serial buffer:
+        packetBuff.push(SerialDecode());    // add serial buffer command to packet buffer
+      }
+    }
   }
+
 }
+//   if (rx.available()) { // TODO -> if packet is "done" -- marked by a flag -> delete and move on. if not, decode 1 packet & add to buffer, send 1 telem packet, check for done, wait some seconds
+//     ControlPacket * control = SerialDecode();
+//     control->resolve(&ROBOCLAW_1, &ROBOCLAW_2);
+//     delete control;
+//   }
+// }
+
+
+
 
 ControlPacket* SerialDecode () {
-  delay(2*1000); // timing delay for my sake
-  Serial.println("Decoding!");
 
   size_t recievePOS = 0; // stores position of recieving buffer
   char ID; // stores ID of current packet decoder
   ControlPacket * control = nullptr; // pointer to decoded packet
 
   recievePOS = rx.rxObj(ID, recievePOS); // store ID char
-  
 
   // decode ID char into specific packet
-
   if (ID == 'R') { // Raw Data
-    Serial.println("R");
     size_t datasize = 4; // sets size of data in the packet
     float data[datasize] = {0}; // creates an empty array of datasize
     for(size_t i = 0; i < datasize; i++) { // takes data from serial port and adds it to the packet
       recievePOS = rx.rxObj(data[i], recievePOS);
-      Serial.println(data[i]);
     }
-    Serial.println("Error");
     control = new Raw(data); // creates new packet of type Raw
+
   } else if (ID == 'V') { // Velocity Data
-    Serial.println("Vel PID");
     size_t datasize = 1;
     float data[datasize] = {0};
     for(size_t i = 0; i < datasize; i++) {
       recievePOS = rx.rxObj(data[i], recievePOS);
-      Serial.println(data[i]);
     }
-    Serial.println("Error");
     control = new VelPID(data);
+
   } else if (ID == 'D') { // Distance / Position Data
-    Serial.println("D");
     size_t datasize = 2;
     float data[datasize] = {0};
     for(size_t i = 0; i < datasize; i++) {
       recievePOS = rx.rxObj(data[i], recievePOS);
-      Serial.println(data[i]);
     }
-    Serial.println("Error");
     control = new PosPID(data);
+    
   } else { // Base case -- currently raw data
     float data[4] = {32,32,0,0};
     control = new Raw(data);
