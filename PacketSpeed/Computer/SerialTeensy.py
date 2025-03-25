9a8db377063beafa3118af481b53eec48af4bf55
Cameron H <enderman9523@gmail.com>
2025-03-24 20:56:51 -0600

    changed serial

--- PacketSpeed/Computer/SerialTeensy.py
+++ PacketSpeed/Computer/SerialTeensy.py
@@ -14,12 +14,12 @@ if __name__ == "__main__":
         serial.open()
     
     while True:
-        ID = 'D'
+        ID = 'V'
         datasize = 0
         datasize = serial.tx_obj(ID,start_pos=datasize,val_type_override='c')
-        datasize = serial.tx_obj(1000,start_pos=datasize,val_type_override="i")
         val = int(input("Enter Val: "))
         for i in range(1):
+            datasize = serial.tx_obj(val,start_pos=datasize,val_type_override="i")
             datasize = serial.tx_obj(val,start_pos=datasize,val_type_override="i")
         serial.send(datasize)
         print(datasize)
