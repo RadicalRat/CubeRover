299c1f08c9b7d35ea54ea87fccc9feb01e774858
Lexi-jh <lexi.j.hanlon@gmail.com>
2025-03-26 11:37:33 -0600

    converted to encoder counts

--- Pi/InputConverter.py
+++ Pi/InputConverter.py
@@ -11,17 +11,26 @@ class ValConverter:
         # self.angle = np.arctan2(rY, rX) #radians from [-pi to pi]
         # squared = rY**2 + rX**2
         # self.speed = np.sqrt(squared)/np.sqrt(2) * .1 #max output is sqrt(2), so on a scale from 0-.1 m/s
+
+
         self.angle  = 0 #change obvi
         self.speed = abs(rX)*100
         
 
     def vel_calc(self, trig):
-        #10 cm/s fastest speed. wheel radius 6 inches. 
+        #10 cm/s fastest speed. wheel radius 7.5 inches. 
         #1 inch is 2.54 cm
         #need rotations per minute, rpm * 2pi / 60 = w
         #v=wr
         #trig vals are 0-2
-        return trig/2 * 100
+        radme = 7.5 * 2.54/100
+        angspeed = .1/radme
+        maxrpm = angspeed * 60/(2*np.pi)
+        maxencoder = maxrpm*28
+        return trig/2 * maxencoder
+
+
+        #return trig/2 * 100
     
 
 rX = 1
